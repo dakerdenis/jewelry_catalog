80f5704b5c1fcc8ad99c8a737f479b8822372a02
dakerdenis <dakerdenis@gmail.com>
2025-08-28 17:35:45 +0400

    added

--- jewelry_shop/catalog/views.py
+++ jewelry_shop/catalog/views.py
@@ -1,16 +1,21 @@
+# catalog/views.py
 from django.shortcuts import render, get_object_or_404
-from .models import Collection, Product
+from django.db.models import Prefetch
+from .models import Collection, Product, ProductImage
 
 def landing_view(request):
-    # TODO: в будущем сюда подтянем «избранные» коллекции/фото
     return render(request, "catalog/landing.html", {})
 
 def product_list_view(request):
-    # TODO: позже добавим фильтры, пагинацию, сортировку
-    return render(request, "catalog/product_list.html", {})
+    # Все активные товары + категории + фотки (для превью)
+    products = (
+        Product.objects.filter(is_active=True)
+        .select_related("category", "category__collection")
+        .prefetch_related("images")   # хватит для первой фотки
+        .order_by("name")
+    )
+    return render(request, "catalog/product_list.html", {"products": products})
 
 def product_detail_view(request, slug: str):
-    # TODO: позже полноценные данные, похожие товары, галерея
-    # Заглушка: попытаемся найти продукт по slug, иначе 404 (это уже полезно)
     product = get_object_or_404(Product.objects.only("id", "name", "slug"), slug=slug)
     return render(request, "catalog/product_detail.html", {"product": product})
